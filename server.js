163bc2ac0a7bbbbc04f40ef1ba83d1ef844a6e4d
bari0205 <akbariilham129@gmail.com>
2020-10-31 05:48:28 +0700

    Bahan Deploy

--- server.js
+++ server.js
@@ -20,6 +20,6 @@ app.use(cors());
 app.use("/api/v1/", routerDb);
 
 //define the server port
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
